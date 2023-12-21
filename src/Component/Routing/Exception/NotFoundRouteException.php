158b52b7eeb49647a66925e30cf89dde20773636
Jean Claude <jeanyao@ymail.com>
2023-12-21 11:18:15 +0500

    Add Route not found exception

---
+++ src/Component/Routing/Exception/NotFoundRouteException.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Laventure\Component\Routing\Exception;
+
+
+/**
+ * NotFoundRouteException
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  Laventure\Component\Routing\Exception
+ */
+class NotFoundRouteException extends \Exception
+{
+     public function __construct(string $path)
+     {
+         parent::__construct("Route $path can not found.", 404);
+     }
+}
