7006f9b559abab83a7f1c2c50c55ce28713a3fb3
Jean-Claude <jeanyao@ymail.com>
2023-12-24 01:25:15 +0300

    Implementation PSR-17

--- src/Component/Http/Utils/Contract/ParameterInterface.php
+++ src/Component/Http/Utils/Contract/ParameterInterface.php
@@ -11,7 +11,7 @@ namespace Laventure\Component\Http\Utils\Contract;
  *
  * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
  *
- * @package  Laventure\Component\Http\Parameter\Contract
+ * @package  Laventure\Component\Http\Utils\Contract
 */
 interface ParameterInterface
 {
