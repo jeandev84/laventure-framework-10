e0abb23b44bfe89f1e5a2f53c18a42c8c1a9061f
Jean-Claude <jeanyao@ymail.com>
2023-12-23 21:18:33 +0300

    Fix response body

--- src/Component/Http/Message/Response/Body/ResponseBody.php
+++ src/Component/Http/Message/Response/Body/ResponseBody.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Laventure\Component\Http\Message\Response\Body;
 
+use Laventure\Component\Http\Message\Stream\Exception\StreamException;
 use Laventure\Component\Http\Message\Stream\Stream;
 
 /**
@@ -17,8 +18,56 @@ use Laventure\Component\Http\Message\Stream\Stream;
  */
 class ResponseBody extends Stream
 {
-    public function __construct(string $resource = '', string $accessMode = 'w')
+
+    /**
+     * @var string|null
+    */
+    protected ?string $content = null;
+
+
+
+    /**
+     * @param string $resource
+     *
+     * @throws StreamException
+    */
+    public function __construct(string $resource = '')
     {
-        parent::__construct($resource ?: 'php://output', $accessMode);
+        parent::__construct($resource ?: 'php://output', 'w');
+    }
+
+
+    /**
+     * @param string $string
+     * @return int
+    */
+   public function write(string $string): int
+   {
+       ob_start();
+       parent::write($string);
+       $this->content = ob_get_clean();
+       return 1;
+   }
+
+
+
+
+    /**
+     * @return string
+    */
+    public function __toString(): string
+    {
+        return $this->content;
     }
 }
+
+
+/*
+$stdout = fopen('php://stdout', 'w');
+ob_start();
+echo "echo output\n";
+fwrite($stdout, "FWRITTEN\n");
+echo "Also echo\n";
+$out = ob_get_clean();
+echo $out;
+*/
