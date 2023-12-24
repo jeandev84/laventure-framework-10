4434f8107db5f093f7a2e8b72932900b9c25e63b
Jean-Claude <jeanyao@ymail.com>
2023-12-24 04:40:52 +0300

    Buffering

---
+++ tests/phpunit/App/Middlewares/BufferMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPUnitTest\App\Middlewares;
+
+
+/**
+ * BuffurerMiddleware
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  PHPUnitTest\App\Middlewares
+ */
+class BufferMiddleware
+{
+    public function __invoke($request, callable $next)
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
