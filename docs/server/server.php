2c73ecd94794410018db957ab4d8eab9fdcd9c8a
Jean Claude <jeanyao@ymail.com>
2024-01-09 20:23:05 +0500

    HTTP Client

--- docs/server/server.php
+++
@@ -1,3 +0,0 @@
-<?php
-# shell_exec('php -S localhost:8000 -t public -d display_errors=1 -d opcache.enable_cli=1');
-shell_exec('env ENV=dev php -S localhost:8000 -t public -d display_errors=1 -d opcache.enable_cli=1');
